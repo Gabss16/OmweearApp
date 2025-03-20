c8ad2dc334a16992c9decbd4bc488a0360cf62f6
Your Name <you@example.com>
2025-03-20 14:52:13 -0600

    first commit

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+import express from "express";
+
+const app = express();
+
+
+app.use(express.json());
+
+
+//Empieza CRUD
+//1.definir rutas de funciones
+
+
+
+
+
+//antes de crud
+export default app;
