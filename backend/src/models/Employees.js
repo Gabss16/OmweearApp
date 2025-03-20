f721ca6f8ebff6e91cd67b43962a23c16e2270fa
Your Name <you@example.com>
2025-03-20 15:38:19 -0600

    crud emplyees listo, carrito de compras a medias, si funciona ni  idea

---
+++ backend/src/models/Employees.js
@@ -0,0 +1,54 @@
+import { Schema, model } from "mongoose";
+
+
+const employeesShema = new Schema({
+name: {
+    type: String,
+    required: true 
+},
+   email: { 
+    type: String,
+    required: true, 
+    unique: true 
+},
+   password: { 
+    type: String, 
+    required: true 
+},
+   phone: { 
+    type: String, 
+    required: true 
+},
+   dui: { 
+    type: String, 
+    required: true, 
+    unique: true 
+},
+   isss: { 
+    type: Number, 
+    required: true 
+},
+   charge: { 
+    type: String, 
+    required: true 
+},
+   profilePhoto: { 
+    type: String
+ },
+   hireDate: {
+     type: Date, 
+     required: true 
+    },
+   birthday: { 
+    type: Date, 
+    required: true },
+   gender: { 
+    type: String, 
+    required: true 
+}
+},{
+    timestamps: true,
+    strict: false
+});
+
+export default model( "Employees", employeesShema)
